9f7b7f767dee6ebce1c4d9233aa472d37be4b080
Кирилл Румянцев <kirillsonytablet@gmail.com>
2023-03-01 12:45:33 +0300

    Commit-4: Создал структуру для api/urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import signup, token
+from rest_framework import routers
 
+from .views import (UserViewSet, signup, token)
+
+
+v1 = routers.DefaultRouter()
+v1.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
+    path('v1/', include(v1.urls)),
     path('v1/auth/signup/', signup, name='signup'),
     path('v1/auth/token/', token, name='login'),
 ]
