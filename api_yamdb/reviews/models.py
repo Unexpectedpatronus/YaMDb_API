ef6e0b03573ae28a3d9230037e1bba822b9cdac3
Evgeny Odintsov <ev1genial@gmail.com>
2023-03-05 01:22:57 +0300

    Заполнил reviews/admin.py, добавил выгрузку в БД при помощи команды: python manage.py runscript load_titles

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
-
 from django.db import models
 
 ROLE_CHOICES = (
@@ -122,31 +120,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-
-
-class Review(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE)
-    title = models.ForeignKey(
-        Title, related_name='reviews', on_delete=models.CASCADE)
-    text = models.TextField()
-    pub_date = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
-    score = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
-
-
-    def __str__(self):
-        return self.name
-
-
-class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE,)
-    review = models.ForeignKey(
-        Review, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
-    pub_date = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
-    
-    def __str__(self):
-        return self.name
