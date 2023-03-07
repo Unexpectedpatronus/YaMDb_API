9075b790d90c822ee7ea94e277236575ba1a1850
Кирилл Румянцев <kirillsonytablet@gmail.com>
2023-03-07 13:02:57 +0300

    Commit-19: Настроил permission для отзывов и комментариев

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,57 +1,46 @@
 from rest_framework import permissions
 
 
-class IsRoleAdmin(permissions.BasePermission):
-
+class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         user = request.user
-        return (
-            user.is_authenticated and user.is_admin
-            or user.is_superuser
-        )
-
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-        return (
-            user.is_authenticated and user.is_admin
+        return bool(
+            user.is_authenticated and user.is_staff
             or user.is_superuser
         )
 
 
-class IsRoleModerator(permissions.BasePermission):
-
+class IsModerator(permissions.BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        return (
-            user.is_authenticated and user.is_moderator
-            or user.is_staff
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+                )
 
     def has_object_permission(self, request, view, obj):
         user = request.user
-        return (
-            user.is_authenticated and user.is_moderator
-            or user.is_staff
+        if request.method == 'DELETE' and user.is_moderator:
+            return True
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return bool(
+            user.is_authenticated and user.is_staff
+            or user.is_superuser
         )
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-
     def has_permission(self, request, view):
-        user = request.user
-        return (
-            user.is_authenticated and user.is_user
-            or request.method in permissions.SAFE_METHODS
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+                )
 
     def has_object_permission(self, request, view, obj):
-        return (
-            obj.author == request.user
-            or request.method in permissions.SAFE_METHODS
-        )
-
-
-class ReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
