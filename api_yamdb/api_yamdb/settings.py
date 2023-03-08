75d1a980571da01aaa3a08b6440e88c44468fe48
Кирилл Румянцев <kirillsonytablet@gmail.com>
2023-03-08 19:14:55 +0300

    Commit-25: Добавил filters.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'django_filters',
     'rest_framework',
     'rest_framework_simplejwt',
     'reviews.apps.ReviewsConfig',
